ff5880fa459ebdedcc36d87fa892b1222eed99cc
Álvaro Zamora <alvarozajim@gmail.com>
2020-03-06 13:41:42 +0100

    clients array, uuid, on connect

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid/v4');
 var express = require('express'),
     app = express(),
     http = require('http'),
@@ -5,19 +6,28 @@ var express = require('express'),
     server, io;
 
 app.get('/', function (req, res) {
-    res.send('listening on 5000');
+    res.send('listening for sockets on port 5000');
 });
 
 server = http.Server(app);
 server.listen(5000);
+console.log('socket server running on port 5000')
 
 io = socketIO(server);
 
-io.on('connection', function (socket) {
-    console.log("client conn")
+var clients = []
 
-    socket.on('disconnect', function (message) {
+io.on('connection', function (socket) {
+
+    socket.on('connect', function (){
+        console.log('client joined')
+        socket.id = uuid();
+        clients.push(socket.id)
+    });
+
+    socket.on('disconnect', function () {
         console.log("client left");
+        clients.filter(item => item !== socket.id);
     });
 
     socket.on('message', function (message) {
