87b2994e03b1fb9b855c25e1d8c9164ce3199f0d
Álvaro Zamora <alvarozajim@gmail.com>
2020-03-06 17:13:33 +0100

    removed unnecessary array and prepared ready

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const crypto = require('crypto');
 const axios = require('axios')
-const { v4: uuid } = require('uuid');
 var express = require('express'),
     app = express(),
     http = require('http'),
@@ -17,20 +16,27 @@ console.log('socket server running on port 5000')
 
 io = socketIO(server);
 
-var clients = []
+var waiting = []
 
 io.on('connection', function (socket) {
 
     console.log('client joined')
+    console.log(Object.keys(io.sockets.connected).length)
 
     socket.on('join', function (name, password){
-        socket.id = uuid();
-        clients.push(socket.id)
 
         var user_password = crypto.createHash('sha256').update(password).digest('base64');
 
         socket.name = name
         socket.password = user_password
+
+        if(Object.keys(io.sockets.connected).length > 2 && waiting.length > 0){
+            io.to(`${socketId}`).emit('ready')
+            // io.to(waiting.shift().id).emit('ready') remove oldest waiting socket and ready him
+            // check if you can emit to with socket.id
+        } else {
+            waiting.push(socket)
+        }
 
         axios.post('http://localhost/laravelrestapi/public/api/users', {
             name: name,
@@ -47,11 +53,10 @@ io.on('connection', function (socket) {
 
     socket.on('disconnect', function () {
         console.log("client left");
-        clients.splice(clients.indexOf(socket.id), 1)
     });
 
     socket.on('message', function (message) {
         console.log(socket.name+": "+ message);
-        console.log(clients)
+        io.to(`${socketId}`).emit('message', socket.name+": "+ message);
     });
 });
