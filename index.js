fe058b8fbad6d20100f0a8b8273b179a5d95f07c
Álvaro Zamora <alvarozajim@gmail.com>
2020-03-12 12:26:10 +0100

    remove log

--- index.js
+++ index.js
@@ -58,8 +58,6 @@ io.on('connection', function (socket) {
         })
         .then((res) => {
 
-            console.log(res)
-
             io.emit('roomUpdate')
 
             room_clients_count = io.sockets.adapter.rooms[room_name].length
